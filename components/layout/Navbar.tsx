9184668dee9bc885cd43fdcf0f546e68c3556bb2
Joshua-Chu <jpchu@up.edu.ph>
2021-10-11 21:45:56 +0800

    Navbar component cleanup

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,10 +1,6 @@
 import Link from "next/link";
-//Components
 import TogglerIcon from "./TogglerIcon";
-
-// Stylings
-import styled from "styled-components";
-import { FlexContainer } from "../../styles/util.styles";
+import { Header, StyledNavBar, Logo, Toggler } from "./Navbar.styles";
 
 type Props = {
 	isDark: boolean;
@@ -31,35 +27,3 @@ const NavBar: React.FC<Props> = ({ isDark, onThemeToggle }) => {
 };
 
 export default NavBar;
-
-const Header = styled.div`
-	background-color: ${(props) => props.theme.accent};
-	color: ${(props) => props.theme.fontColor};
-	padding: 0 20px;
-	box-shadow: rgb(0 0 0 / 5%) 0px 3px 2px 0px;
-`;
-const StyledNavBar = styled(FlexContainer)`
-	padding: 30px 0;
-	justify-content: space-between;
-`;
-
-const Logo = styled.h3`
-	margin: 0;
-	font-weight: 800;
-	font-size: 1.2rem;
-
-	@media only screen and (max-width: 500px) {
-		font-size: 0.8rem;
-	} ;
-`;
-
-const Toggler = styled.div`
-	margin: 0;
-	font-weight: 800;
-	font-size: 1.2rem;
-	display: flex;
-	align-items: center;
-	@media only screen and (max-width: 500px) {
-		font-size: 0.8rem;
-	}
-`;
