b2c382b2a03f0793713fe22c52094c57e779b445
Joshua-Chu <jpchu@up.edu.ph>
2021-10-11 11:10:27 +0800

    Adding responsive styling on details page

--- components/Countries/SingleCountryPage.tsx
+++ components/Countries/SingleCountryPage.tsx
@@ -10,7 +10,7 @@ const SingleCountryPage: React.FC<Props> = ({ country }) => {
 	return (
 		<StyledSingleCountryPage>
 			<CountryFlagContainer>
-				<Image src={country.flags.svg} alt="flag" layout="fill" className="image" />
+				<Image src={country.flags.svg} alt="flag" layout="fill" className="image" objectFit="contain" />
 			</CountryFlagContainer>
 			<SingleCountryDetailsPage country={country} />
 		</StyledSingleCountryPage>
@@ -23,32 +23,17 @@ const StyledSingleCountryPage = styled(FlexContainer)`
 	margin-top: 80px;
 	padding: 0px 20px;
 	justify-content: flex-start;
-	align-items: flex-start;
-
+	align-items: center;
 	@media only screen and (max-width: 800px) {
 		flex-direction: column;
-		justify-content: flex-start;
-		align-items: center;
 	}
 `;
 const CountryFlagContainer = styled.div`
+	position: relative;
 	width: 560px;
-	> div {
-		position: unset !important;
-	}
-	.image {
-		object-fit: contain;
-		width: 100% !important;
-		position: relative !important;
-		height: 400px !important;
-	}
-
+	height: 400px;
 	@media only screen and (max-width: 800px) {
-		flex-basis: 100%;
 		width: 100%;
-
-		.image {
-			height: unset !important;
-		}
+		height: 450px;
 	}
 `;
